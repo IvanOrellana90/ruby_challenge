15ed21c8e36063be3faef02b3512480e804ec4f3
IvanOrellana90 <ivan990@gmail.com>
2021-08-05 15:22:12 -0400

    terminal class created

---
+++ terminal.rb
@@ -0,0 +1,19 @@
+require_relative "product.rb"
+
+class Terminal
+
+    def initialize
+        @shopping_list = []
+        @product_list = []
+    end
+
+    attr_accessor :shopping_list, :product_list
+
+    def scan(code)
+
+    end
+
+    def total()
+
+    end
+end
