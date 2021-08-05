0c38e80a81d25432c25dee75a40f0c852fee322c
IvanOrellana90 <ivan990@gmail.com>
2021-08-05 16:12:14 -0400

    added loadProductList and scan method to terminal class

--- terminal.rb
+++ terminal.rb
@@ -3,17 +3,29 @@ require_relative "product.rb"
 class Terminal
 
     def initialize
-        @shopping_list = []
+        @shopping_list = {}
         @product_list = []
     end
 
     attr_accessor :shopping_list, :product_list
 
+    def loadProductList()
+        @product_list.push(Product.new("A", 2.00, true, 4, 7.00))
+        @product_list.push(Product.new("B", 12.00))
+        @product_list.push(Product.new("C", 1.25, true, 6, 6.00))
+        @product_list.push(Product.new("D", 0.15))
+    end
+
     def scan(code)
-
+        for product in @product_list do
+            if product.code() == code
+                if @shopping_list[code].nil?
+                    @shopping_list[code] = 1
+                else
+                    @shopping_list[code] += 1
+                end
+            end
+        end
     end
 
-    def total()
-
-    end
 end
